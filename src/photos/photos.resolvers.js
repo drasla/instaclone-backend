9e43fc356db79580233ea9aa909b66902131c2b0
Tae.Garam <drasla@naver.com>
2021-06-16 22:43:17 +0900

    #6.6 seeHashTag

--- src/photos/photos.resolvers.js
+++ src/photos/photos.resolvers.js
@@ -2,5 +2,14 @@ export default {
     Photo: {
         user: ({userId}) => client.user.findUnique({ where: { id: userId }}),
         hashtags: ({id}) => client.hashtag.findMany({ where: { photos: { some: {id}}}})
+    },
+    hashtag: {
+        totalPhotos: ({id}) => client.photo.count({ where: {hashtags: { some: {id}}}}),
+        photos: ({id}, {page}, { loggedInUser }) => {
+            return client.hashtag.findUnique({ where: {id}})
+                .photos();
+        }
+
     }
+
 }
