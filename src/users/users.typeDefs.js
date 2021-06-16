68a9a95108916453b32aecd7a5f1d5b5be7ea131
Tae.Garam <drasla@naver.com>
2021-06-16 18:36:02 +0900

    #4.27 Computed Fields part Two

--- src/users/users.typeDefs.js
+++ src/users/users.typeDefs.js
@@ -11,7 +11,12 @@ export default gql`
         avatar: String
         following: [User]
         followers: [User]
+        totalFollowing: Int!
+        totalFollowers: Int!
+        isMe: Boolean!
         createdAt: String!
         updatedAt: String!
     }
 `;
+
+// isFollowing: Boolean!
