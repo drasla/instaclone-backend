4dbdf53ca4a9ac4dc701b8f51bd4dbe5735a27fe
Tae.Garam <drasla@naver.com>
2021-06-18 16:22:46 +0900

    #6.13 Comment on Photos

---
+++ src/comments/createComment/createComment.resolvers.js
@@ -0,0 +1,37 @@
+import {protectResolver} from "../../users/users.utils";
+import client from "../../client";
+
+export default {
+    Mutation: {
+        createComment: protectResolver(async (_, {photoId, payload}, {loggedInUser}) => {
+            const ok = await client.photo.findUnique({
+                where: { id: photoId },
+                select: { id: true }
+            });
+            if(!ok) {
+                return {
+                    ok: false,
+                    error: "Photo not Found."
+                }
+            }
+            await client.comment.create({
+                data: {
+                    payload,
+                    photo: {
+                        connect: {
+                            id: photoId
+                        }
+                    },
+                    user: {
+                        connect: {
+                            id: loggedInUser.id
+                        }
+                    }
+                }
+            });
+            return {
+                ok: true
+            };
+        })
+    }
+};
