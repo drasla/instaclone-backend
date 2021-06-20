4199e652af93186d18fb4aec725a618da6407f12
Tae.Garam <drasla@naver.com>
2021-06-20 16:29:21 +0900

    #7.9 Filtering Subscriptions part One

--- src/messages/roomUpdates/roomUpdates.resolvers.js
+++ src/messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,10 +1,16 @@
 import pubsub from "../../pubsub";
 import {NEW_MESSAGE} from "../../constants";
+import {withFilter} from "apollo-server-express";
 
 export default {
     Subscription: {
         roomUpdates: {
-            subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+            subscribe: withFilter(
+                () => pubsub.asyncIterator(NEW_MESSAGE),
+                (payload, {id}) => {
+                    return payload.roomUpdates.roomId === id;
+                }
+            )
         }
     }
 }
